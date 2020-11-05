8498f444c884cd3f0fc7f798d2a34993cd64e940
thcl-0407 <thcloc@icloud.com>
2020-11-05 17:12:59 +0700

    check if email exists

--- controller/note_contrl.js
+++ controller/note_contrl.js
@@ -18,24 +18,47 @@ async function GetUserbyId(Id, CallBack){
     }
 }
 
-async function AddUser(User, CallBack){
+async function GetUserbyEmail(Email, CallBack){
     await mssql.poolConnection
     try{
         const request = mssql.pool.request()
         const result = await request
-        .input('first_name', mssql.mssql.NVarChar, User.firstName)
-        .input('last_name', mssql.mssql.NVarChar, User.lastName)
-        .input('email', mssql.mssql.VarChar, User.email)
-        .input('password', mssql.mssql.VarChar, bcrypt.hashSync(User.password, bcrypt.genSaltSync(10)))
-        .query('INSERT INTO [NOTE_WORK].[dbo].[User](first_name, last_name, email, password) VALUES (@first_name, @last_name, @email, @password)')
-        if(result.rowsAffected[0] == 1){
-            CallBack(true)
+        .input('email', mssql.mssql.VarChar, Email)
+        .query('SELECT * FROM [NOTE_WORK].[dbo].[User] WHERE email = @email')
+        if(result.rowsAffected[0] > 0){
+            CallBack(true, result.recordset)
+        }else{
+            CallBack(false, null)
+        }
+    }catch{
+        CallBack(false, null)
+    }
+}
+
+async function AddUser(User, CallBack){
+    await GetUserbyEmail(User.email, async (result, data)=>{
+        if(!result){
+            await mssql.poolConnection
+            try{
+                const request = mssql.pool.request()
+                const result = await request
+                .input('first_name', mssql.mssql.NVarChar, User.firstName)
+                .input('last_name', mssql.mssql.NVarChar, User.lastName)
+                .input('email', mssql.mssql.VarChar, User.email)
+                .input('password', mssql.mssql.VarChar, bcrypt.hashSync(User.password, bcrypt.genSaltSync(10)))
+                .query('INSERT INTO [NOTE_WORK].[dbo].[User](first_name, last_name, email, password) VALUES (@first_name, @last_name, @email, @password)')
+                if(result.rowsAffected[0] == 1){
+                    CallBack(true)
+                }else{
+                    CallBack(false)
+                }
+            }catch{
+                CallBack(false)
+            }
         }else{
             CallBack(false)
         }
-    }catch{
-        CallBack(false)
-    }
+    })
 }
 
 async function Login(User, CallBack){
